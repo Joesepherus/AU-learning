d6c79cfac1f7462c0b02a7a75249e5dde06fec16
Joseph Maloch <jozefmaloch@gmail.com>
2024-04-02 01:51:18 +0800

    Week 2 Challenge done.

---
+++ Week 2 Challenge/GiftList/client/index.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const merkleTree = new MerkleTree(niceList);
+
+const serverUrl = "http://localhost:1225";
+const root = merkleTree.getRoot();
+console.log("root: ", root);
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Traci McDermott";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    proof,
+    name,
+  });
+
+  console.log({ gift });
+}
+
+main();
